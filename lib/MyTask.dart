7421fbec3be1ef4acc97f00c2dc75eba42c13d15
David <david.polania3@gmail.com>
2018-10-01 13:09:06 -0500

    Mostrar la lista de tareas guardadas

--- lib/MyTask.dart
+++ lib/MyTask.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:todolist/addTask.dart';
+import 'package:todolist/editTask.dart';
 import 'package:todolist/main.dart';
 
 class MyTask extends StatefulWidget {
@@ -16,7 +17,6 @@ class MyTask extends StatefulWidget {
 }
 
 class _MyTaskState extends State<MyTask> {
-
   void _signOut() {
     AlertDialog alertDialog = new AlertDialog(
       content: Container(
@@ -82,11 +82,10 @@ class _MyTaskState extends State<MyTask> {
     return Scaffold(
       floatingActionButton: new FloatingActionButton(
         onPressed: () {
-
           Navigator.of(context).push(new MaterialPageRoute(
-              builder: (BuildContext context) => new AddTask())
-          );
-
+              builder: (BuildContext context) => new AddTask(
+                    email: widget.user.email,
+                  )));
         },
         child: Icon(Icons.add),
         backgroundColor: Color.fromRGBO(3, 81, 57, 1.0),
@@ -99,103 +98,196 @@ class _MyTaskState extends State<MyTask> {
           children: <Widget>[],
         ),
       ),
-      body: Container(
-        height: 170.0,
-        width: double.infinity,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                // TODO: poner imagen de fondo
-                image: new AssetImage("img/bgpattern.png"),
-                fit: BoxFit.cover),
-            boxShadow: [new BoxShadow(color: Colors.black, blurRadius: 8.0)],
-            color: Color.fromRGBO(3, 81, 57, 100.0)),
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    width: 60.0,
-                    height: 60.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          image: new NetworkImage(widget.user.photoUrl),
-                          fit: BoxFit.cover),
+      body: new Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 160.0),
+            child: StreamBuilder(
+              stream: Firestore.instance
+                  .collection("task")
+                  .where("email", isEqualTo: widget.user.email)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData)
+                  return new Container(
+                    child: Center(
+                      child: CircularProgressIndicator(),
                     ),
-                  ),
-                  new Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 18.0),
-                      child: new Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          new Text(
-                            "Bienvenido",
-                            style: new TextStyle(
-                                fontSize: 18.0, color: Colors.white),
-                          ),
-                          new Text(
-                            widget.user.displayName,
-                            style: new TextStyle(
-                                fontSize: 24.0, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  new IconButton(
-                    icon: Icon(
-                      Icons.exit_to_app,
-                      color: Colors.white,
-                      size: 30.0,
-                    ),
-                    onPressed: () {
-                      _signOut();
-                    },
-                  )
-                ],
-              ),
+                  );
+
+                return new TaskList(
+                  document: snapshot.data.documents,
+                );
+              },
             ),
-            new Text(
-              "Mis Tareas",
-              style: new TextStyle(
-                  color: Colors.white,
-                  fontSize: 30.0,
-                  letterSpacing: 1.0,
-                  fontFamily: "Segoe"),
-            )
-          ],
-        ),
+          ),
+          Container(
+            height: 170.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+
+                    // TODO: poner imagen de fondo
+
+                    image: new AssetImage("img/bgpattern.png"),
+                    fit: BoxFit.cover),
+                boxShadow: [
+                  new BoxShadow(color: Colors.black, blurRadius: 8.0)
+                ],
+                color: Color.fromRGBO(3, 81, 57, 100.0)),
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        width: 60.0,
+                        height: 60.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: new NetworkImage(widget.user.photoUrl),
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                      new Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 18.0),
+                          child: new Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              new Text(
+                                "Bienvenido",
+                                style: new TextStyle(
+                                    fontSize: 18.0, color: Colors.white),
+                              ),
+                              new Text(
+                                widget.user.displayName,
+                                style: new TextStyle(
+                                    fontSize: 24.0, color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      new IconButton(
+                        icon: Icon(
+                          Icons.exit_to_app,
+                          color: Colors.white,
+                          size: 30.0,
+                        ),
+                        onPressed: () {
+                          _signOut();
+                        },
+                      )
+                    ],
+                  ),
+                ),
+                new Text(
+                  "Mis Tareas",
+                  style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      letterSpacing: 1.0,
+                      fontFamily: "Segoe"),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
-//img/bg.jpg
-/*boxShadow: [new BoxShadow(color: Colors.black, blurRadius: 8.0)]),
-        child: Column(
-          children: <Widget>[
-            new Image.asset(
-              "img/bgpattern.png",
-              width: 300.0,
-            ),
-            Row(
-              children: <Widget>[
-                Container(
-                  width: 500.0,
-                  height: 500.0,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image: NetworkImage(widget.user.photoUrl),
-                        fit: BoxFit.cover)
+class TaskList extends StatelessWidget {
+  TaskList({this.document});
+
+  final List<DocumentSnapshot> document;
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      itemCount: document.length,
+      itemBuilder: (BuildContext contex, int i) {
+        String title = document[i].data['title'].toString();
+        String note = document[i].data['note'].toString();
+        String duedate = document[i].data['duedate'].toString();
+
+        return Padding(
+          padding: const EdgeInsets.only(left: 16.0, top: 16.0, bottom: 8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Text(
+                          title,
+                          style:
+                              new TextStyle(fontSize: 20.0, letterSpacing: 1.0),
+                        ),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16.0),
+                            child: Icon(
+                              Icons.date_range,
+                              color: Colors.green,
+                            ),
+                          ),
+                          Text(
+                            duedate,
+                            style: new TextStyle(
+                              fontSize: 18.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16.0),
+                            child: Icon(
+                              Icons.note,
+                              color: Colors.green,
+                            ),
+                          ),
+                          Expanded(
+                              child: Text(
+                            note,
+                            style: new TextStyle(
+                              fontSize: 18.0,
+                            ),
+                          )),
+                        ],
+                      ),
+                    ],
                   ),
-                )
-              ],
-            ),
-          ],
-        ),*/
+                ),
+              ),
+            /* new IconButton(
+                  icon: Icon(Icons.edit, color: Colors.green),
+                  onPressed: () {
+                    Navigator.of(context).push(new MaterialPageRoute(
+                        builder: (BuildContext context)=> new EditTask())
+                    );
+                  },
+                  )*/
+            ] ,
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/editTask.dart
+++ lib/editTask.dart
@@ -1,22 +1,24 @@
-import 'dart:async';
+/*import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AddTask extends StatefulWidget {
-  AddTask({this.email});
+class  EditTask extends StatefulWidget {
+  EditTask({this.email});
+
   final String email;
+
   @override
-  _AddTaskState createState() => _AddTaskState();
+  _EditTaskState createState() => _EditTaskState();
 }
 
-class _AddTaskState extends State<AddTask> {
-  
+class _EditTaskState extends State<EditTask> {
   DateTime _dueDate = new DateTime.now();
   String _dateText = '';
 
   String newTask = '';
   String note = '';
+
   Future<Null> _selectDueDate(BuildContext context) async {
     final picked = await showDatePicker(
         context: context,
@@ -24,26 +26,15 @@ class _AddTaskState extends State<AddTask> {
         firstDate: DateTime(2018),
         lastDate: DateTime(2080));
 
-    if(picked != null){
+    if (picked != null) {
       setState(() {
-        _dueDate=picked;
+        _dueDate = picked;
         _dateText = "${picked.day}/${picked.month}/${picked.year}";
       });
     }
   }
 
-  void _addData(){
-    Firestore.instance.runTransaction((Transaction transaction) async{
-      CollectionReference reference = Firestore.instance.collection('task');
-      await reference.add({
-          "email" : widget.email,
-        "title": newTask,
-        "duedate": _dateText,
-        "note": note,
-      });
-    });
-    Navigator.pop(context);
-  }
+
 
   @override
   void initState() {
@@ -62,40 +53,47 @@ class _AddTaskState extends State<AddTask> {
             width: double.infinity,
             decoration: BoxDecoration(
               image: DecorationImage(
-                  image:  AssetImage("img/bgpattern.png"), fit: BoxFit.cover
-              ),
-                color: Color.fromRGBO(3, 81, 57, 1.0),
+                  image: AssetImage("img/bgpattern.png"), fit: BoxFit.cover),
+              color: Color.fromRGBO(3, 81, 57, 1.0),
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text("Mis tareas",style: new TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-                letterSpacing: 1.0,
-                fontFamily: "Segoe"),
+                Text(
+                  "Mis tareas",
+                  style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      letterSpacing: 1.0,
+                      fontFamily: "Segoe"),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 20.0),
-                  child: Text("Añadir Tarea", style: new TextStyle( fontSize: 24.0, color: Colors.white),),
+                  child: Text(
+                    "Editar Tarea",
+                    style: new TextStyle(fontSize: 24.0, color: Colors.white),
+                  ),
                 ),
-                Icon(Icons.list, color: Colors.white, size: 30.0,),
+                Icon(
+                  Icons.list,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
               ],
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
-              onChanged: (String str){
+              onChanged: (String str) {
                 setState(() {
-                  newTask=str;
+                  newTask = str;
                 });
               },
               decoration: new InputDecoration(
-                icon: Icon(Icons.dashboard),
-                hintText: "Nueva Tarea",
-                border: InputBorder.none
-              ),
+                  icon: Icon(Icons.dashboard),
+                  hintText: "Nueva Tarea",
+                  border: InputBorder.none),
               style: new TextStyle(fontSize: 22.0, color: Colors.black),
             ),
           ),
@@ -104,51 +102,61 @@ class _AddTaskState extends State<AddTask> {
             child: new Row(
               children: <Widget>[
                 Padding(
-                  padding: const EdgeInsets. only(right: 16.0),
+                  padding: const EdgeInsets.only(right: 16.0),
                   child: new Icon(Icons.date_range),
                 ),
-                new Expanded(child: Text("Due Date",  style: new TextStyle(fontSize: 22.2, color: Colors.black54),)),
-               new FlatButton(
-                   onPressed: ()=> _selectDueDate(context),
-                   child: Text(_dateText,  style: new TextStyle(fontSize: 22.2, color: Colors.black54),)),
+                new Expanded(
+                    child: Text(
+                  "Due Date",
+                  style: new TextStyle(fontSize: 22.2, color: Colors.black54),
+                )),
+                new FlatButton(
+                    onPressed: () => _selectDueDate(context),
+                    child: Text(
+                      _dateText,
+                      style:
+                          new TextStyle(fontSize: 22.2, color: Colors.black54),
+                    )),
               ],
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
-              onChanged: (String str){
+              onChanged: (String str) {
                 setState(() {
-                  note=str;
+                  note = str;
                 });
               },
               decoration: new InputDecoration(
                   icon: Icon(Icons.note),
                   hintText: "Nota",
-                  border: InputBorder.none
-              ),
+                  border: InputBorder.none),
               style: new TextStyle(fontSize: 22.0, color: Colors.black),
             ),
           ),
-
           Padding(
             padding: const EdgeInsets.only(top: 100.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-            IconButton(
-                icon: Icon(Icons.check, size: 40.0,),
-                onPressed: (){
-                  _addData();
-                }
-            ),
-            IconButton(
-                icon: Icon(Icons.close, size: 40.0,),
-                onPressed: (){
-                  Navigator.pop(context);
-                }
-            )
-            ],
+                IconButton(
+                    icon: Icon(
+                      Icons.check,
+                      size: 40.0,
+                    ),
+                    onPressed: () {
+                     context
+                    }),
+                IconButton(
+                    icon: Icon(
+                      Icons.close,
+                      size: 40.0,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    })
+              ],
             ),
           )
         ],
@@ -156,3 +164,4 @@ class _AddTaskState extends State<AddTask> {
     );
   }
 }
+*/
